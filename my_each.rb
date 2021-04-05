a8c2f0d06872fc9ffd097696a44b58c001b915d8
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-04-05 22:06:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
 def my_each # put argument(s) here
   # code here
+end 
+def my_each(arr)
+  i = 0
+  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end
+  arr
 end
+
+# arr = [1,2,3,4]
+
+# my_each(arr) do |i|
+#   puts "#{i}"
+# end 

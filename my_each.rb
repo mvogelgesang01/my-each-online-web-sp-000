f592f69cc858553eee97f2eecf8793526606cea7
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-04-05 22:18:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,11 @@
 def my_each # put argument(s) here
-  collection = [1, 2, 3, 4]
+  
+  end
+
+def my_each(arr)
+  ollection = [1, 2, 3, 4]
   my_each(collection) do |i|
     puts i
-  end
-end
-def my_each(arr)
   i = 0
   while i < arr.length
     yield(arr[i])
